28e42b3fcaa6e802bd10b3a404973c23081fa368
archfiend-Tanvir <tanvirelhussain@gmail.com>
2023-09-25 18:12:24 +0100

    UPdates Script

--- Js/Maths_abs/js/script.js
+++ Js/Maths_abs/js/script.js
@@ -1,6 +1,25 @@
 function diffrence(a, b) {
-  return Maths.abs(a - b);
+  return Math.abs(a - b);
 }
 
 console.log(diffrence(3, 5));
 // Expected output 2
+
+/** 
+ * 
+ * 
+function diffrence(a, b) {
+  return Math.floor(a - b);
+}
+
+console.log(diffrence(3, 5));
+//Expected Output -2
+
+// This code gives us the lowest possible number becouse this 
+is maths.floor. So we get the bottom most numbter and the is
+gives us minus 2.
+
+Where as Math.abs would give us 2 (positive 2)
+
+
+*/
