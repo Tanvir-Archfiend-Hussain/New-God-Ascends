3ae5ce3c6fd69e5768041f202eeedcb8b160e40a
archfiend-Tanvir <tanvirelhussain@gmail.com>
2023-09-26 15:22:43 +0100

    UPDATES

---
+++ Js/Concat Array/js/script04.js
@@ -0,0 +1,10 @@
+const Fruits = [
+  "blond twink",
+  "apple ass korean twink",
+  "mature",
+  "thai mature",
+];
+
+Fruits.push("thai boy", "philipenes twink boy");
+
+console.log(Object.keys(Fruits));
---
+++ Js/Concat Array/js/script05.js
@@ -0,0 +1,14 @@
+const Fruits = [
+  "blond twink",
+  "apple ass korean twink",
+  "mature",
+  "thai mature",
+];
+
+Fruits.push("thai boy", "philipenes twink boy");
+Fruits.length = 10;
+
+console.log(Fruits);
+console.log(Object.keys(Fruits));
+console.log(Fruits.length);
+console.log(Fruits[8]);
