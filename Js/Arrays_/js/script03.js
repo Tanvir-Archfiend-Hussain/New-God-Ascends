95efca7fa24e39b5e2c86a0c25260df4b18590db
archfiend-Tanvir <tanvirelhussain@gmail.com>
2023-09-26 14:00:03 +0100

    Updates

---
+++ Js/Arrays_/js/script03.js
@@ -0,0 +1,11 @@
+const Fruits = [
+  "blond twink",
+  "apple ass korean twink",
+  "mature",
+  "thai mature",
+];
+
+Fruits.push("thai boy", "philipenes twink boy");
+
+console.log(Fruits[5]);
+console.log(Fruits[0]);
